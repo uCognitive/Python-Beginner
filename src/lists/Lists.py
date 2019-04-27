120a578e2c9d2a421c8e3ef2a4bee9853083e825
Babar <babar.bashir92@yahoo.com>
2019-04-27 16:54:06 +0500

    Commit date : 27-April-2019    Added some list implementations

---
+++ src/lists/Lists.py
@@ -0,0 +1,23 @@
+user = ['babar', 'bilal', 'ali', 'junaid', 'etc']
+# print list
+print(user)
+
+# print 1st index
+print(user[0])
+
+# print last index
+print(user[-1])
+
+# insert element at end of list
+user.append('ect2')
+print(user)
+
+# modify list by with index
+
+user[-1] = 'etc1'
+print(user)
+
+# remove item from list by index
+
+user.remove('etc1')
+print(user)

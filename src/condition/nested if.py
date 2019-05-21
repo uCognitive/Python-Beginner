af3928cd6bf9b543cc388fadc3e07b873d99fcf4
ucet786 <junaidbashir392@gmail.com>
2019-05-21 17:02:55 +0500

    Commit date : 21-May-2019      updated comment in nested if condition section

---
+++ src/condition/nested if.py
@@ -0,0 +1,28 @@
+# user input
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+num3 = int(input("Enter 3rd number: "))
+
+if num1 > num2: #check if num1 > num2
+
+    if num1 > num3: # also num1 > num3
+
+        # if both condition true execute it
+
+        print("Num1 is greatest")
+
+if num2 > num1:# check if num2 > num1
+
+    if num2 > num3:# also num2 > num3
+
+        #if both conditions are true execute it
+
+        print("Num2 is greatest")
+
+if num3 > num1: #check if num3 > num1
+
+    if num3 > num2: # also num3 > num2
+
+        #if both conditions are true execute it
+        
+        print("Num3 is greatest")

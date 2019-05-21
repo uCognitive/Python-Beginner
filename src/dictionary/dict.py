a199eb5c9f72def1a72ffcab9220feb961ec90a4
ucet786 <junaidbashir392@gmail.com>
2019-05-21 17:03:32 +0500

    Commit date : 21-May-2019      updated comment in dictionary section

---
+++ src/dictionary/dict.py
@@ -0,0 +1,12 @@
+#initializing the dictionary
+student = {'Name':'junaid','Marks':100,'Grade':'A'}
+
+#display keys and their values
+print(student)
+
+# Accessing the value of Name
+print(student['Name'])
+
+#Adding value to dictionary
+student['Status'] = 'Good'
+print(student)

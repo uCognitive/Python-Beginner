750de6c7052146e6b31156cabdc307beaeebc884
ucet786 <junaidbashir392@gmail.com>
2019-05-21 17:02:17 +0500

    Commit date : 21-May-2019      update comment in condition

---
+++ src/condition/if condition.py
@@ -0,0 +1,16 @@
+# initializing variable a value
+
+x = 10
+
+# check if variable is less than 10
+
+if x < 10:
+
+#if true then execute it
+
+    print("You are under 10")
+else:
+
+# if not then execute it
+
+    print("you are 10")

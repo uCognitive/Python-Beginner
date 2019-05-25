fbf461ed2f1795237174766752663022c22c1fb7
ucet786 <junaidbashir392@gmail.com>
2019-05-25 18:05:49 +0500

    Commit date : 25-May-2019

---
+++ src/while loop/table.py
@@ -0,0 +1,5 @@
+num  = int(input("Enter number: ")) # user input
+i = 1 # initialization of loop variable
+while i <= 10: #condition of loop
+    print(num," x ",i," = ",num*i)
+    i = i+1 #increament
